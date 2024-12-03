b299ec4ed3039ba016c0e1ec72b187229d8f9e5f
Grapek05 <tomgra1201@wp.pl>
2024-12-03 09:28:29 +0100

    Update z1.py

---
+++ z1.py
@@ -0,0 +1,14 @@
+def add_user(user_data):
+    pass
+
+def save_users_to_file(users):
+    pass
+
+def validate_nip(nip):
+    pass
+
+def validate_pesel(pesel):
+    pass
+
+def validate_regon(regon):
+    pass
